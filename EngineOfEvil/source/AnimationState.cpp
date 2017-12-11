f743f8d6f9ce8f3f1b25c9087a336cde13eebc1c
Tom Freehill <tmf7@njit.edu>
2017-12-11 11:49:26 -0500

    updated open source license information

--- EngineOfEvil/source/AnimationState.cpp
+++ EngineOfEvil/source/AnimationState.cpp
@@ -1,3 +1,29 @@
+/*
+===========================================================================
+
+Engine of Evil GPL Source Code
+Copyright (C) 2016-2017 Thomas Matthew Freehill 
+
+This file is part of the Engine of Evil GPL game engine source code. 
+
+The Engine of Evil (EOE) Source Code is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+EOE Source Code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with EOE Source Code.  If not, see <http://www.gnu.org/licenses/>.
+
+
+If you have questions concerning this license, you may contact Thomas Freehill at tom.freehill26@gmail.com
+
+===========================================================================
+*/
 #include "AnimationState.h"
 #include "Game.h"
 
--- EngineOfEvil/source/main.cpp
+++ EngineOfEvil/source/main.cpp
@@ -1,13 +1,33 @@
-//**********************************************
-//*
-//*Copyright Thomas Freehill July 29 2016
-//*This is just garbage code I'm using to learn
-//*how I'd like to structure my future code
-//*and to get back in the rhythm of 
-//*readability and perfomance
-//*
-//**********************************************
+/*
+===========================================================================
 
+Engine of Evil GPL Source Code
+Copyright (C) 2016-2017 Thomas Matthew Freehill 
+
+This file is part of the Engine of Evil GPL game engine source code. 
+
+The Engine of Evil (EOE) Source Code is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+EOE Source Code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with EOE Source Code.  If not, see <http://www.gnu.org/licenses/>.
+
+
+If you have questions concerning this license, you may contact Thomas Freehill at tom.freehill26@gmail.com
+
+===========================================================================
+*/
+
+//*************************************************************//
+// Original Copyright (C) Thomas Matthew Freehill July 29 2016 //
+//*************************************************************//
 #include "Game.h"
 
 // DEBUG: not using SDL_main
