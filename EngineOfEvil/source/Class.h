098db0b980df707d87059a42ea51d3291f4b9d76
Tom Freehill <tmf7@njit.edu>
2018-01-22 05:58:39 -0500

    updated RTTI to allow class instancing by class name (to simplify eEntityPrefabManager to just manage spawnArgs per prefab instead of allocated eGameObjects); eGameObject ownership hierarcy in progress (possibly use eHierarcy nodes)

--- EngineOfEvil/source/Class.h
+++ EngineOfEvil/source/Class.h
@@ -29,79 +29,135 @@ If you have questions concerning this license, you may contact Thomas Freehill a
 
 #include "Definitions.h"
 #include "Hierarchy.h"
-#include "DynamicType.h"
 
-
-//****************
+//********************************************************************************
 // ECLASS_DECLARATION
-//
 // This macro must be included in the declaration of any subclass of eClass.
 // It declares variables used in type checking.
-//****************
+//********************************************************************************
 #define ECLASS_DECLARATION( classname )											\
 public:																			\
+	static std::unique_ptr<eClass>			CreateInstance();					\
 	static eDynamicType						Type;								\
-	virtual eDynamicType *					GetType() const;					\
-
-// TODO: should IsType( eDynamicType * type ) be part of eClass and not the macro?
+	virtual eDynamicType *					GetType() const;
 
 
-
-/*
-//****************
-// ECLASS_DECLARATION
-//
-// This macro must be included in the declaration of any subclass of eClass.
-// It declares variables used in type checking.
-//****************
-#define ECLASS_DECLARATION( classname )											\
-public:																			\
-	static	ClassType_t						Type;								\
-	virtual	bool IsClassType(ClassType_t classType) const override;				\
-
-
-//****************
+//********************************************************************************
 // ECLASS_DEFINITION
-// 
 // This macro must be included in the class definition to properly initialize 
 // variables used in type checking. Take special care to ensure that the 
-// proper parentclass is indicated or the run-time type information will be
-// incorrect.
-//****************
-#define ECLASS_DEFINITION(parentclassname, classname)							\
-ClassType_t classname::Type = std::hash<std::string>()(TO_STRING(classname));	\
-bool classname::IsClassType( ClassType_t classType ) const	{					\
-	if( classType == classname::Type )											\
-		return true;															\
-	return parentclassname::IsClassType( classType );							\
+// proper parentclass is indicated or the run-time type information will be incorrect.
+//********************************************************************************
+#define ECLASS_DEFINITION( parentclassname, classname )							\
+eDynamicType classname::Type( #classname,										\
+							  #parentclassname,									\
+							  &classname::CreateInstance,						\
+							  ( void ( eClass::* )() )&classname::Spawn );		\
+																				\
+std::unique_ptr<eClass> classname::CreateInstance() {							\
+	return std::make_unique<classname>();										\
 }																				\
-*/
+																				\
+eDynamicType * classname::GetType() const {										\
+	return &classname::Type;													\
+}
 
 namespace evil {
 
-//using ClassType_t = const std::size_t;
+class eClass;
+
+//*****************************
+//		eDynamicType
+// defines  runtime type information
+// used by the Engine Of Evil class hierarchy
+//*****************************
+class eDynamicType {
+public:
+
+								eDynamicType( const char *classname, const char * parentclassname, eClass *( *CreateInstance )(), void ( eClass::*Spawn )() );
+							   ~eDynamicType();
+
+	void						Init();
+	void						Shutdown();
+	bool						IsType( const eDynamicType & classType ) const;
+
+public:
+
+	eHierarchy<eDynamicType>	classNode;
+	std::string					classname;
+	std::string					parentclassname;
+	eClass *					( *CreateInstance )();
+	void						( eClass::*Spawn )();
+
+	eDynamicType *				parentType;
+	eDynamicType *				nextType;			// for sorting in alphabetical order at startup
+	int							typeNum;
+	int							lastChild;
+	bool						initialized;
+
+};
 
 //*************************************************
 //					eClass
-// base class for all engine of evil game objects
+// base class for all Engine of Evil game objects
 //*************************************************
-class eClass{
-public:
+class eClass {
 
-	virtual					   ~eClass() = default;
-								eClass() = default;
-								eClass(const eClass & other) = default;
-								eClass(eClass && other) = default;
-	eClass &					operator=(const eClass & other) = default;
-	eClass &					operator=(eClass && other) = default;
-
-	virtual bool				IsClassType(ClassType_t classType) const;
+	ECLASS_DECLARATION( eClass )
 
 public:
 
-	static ClassType_t			Type;
+	virtual							       ~eClass() = default;
+											eClass() = default;
+											eClass(const eClass & other) = default;
+											eClass(eClass && other) = default;
+	eClass &								operator=(const eClass & other) = default;
+	eClass &								operator=(eClass && other) = default;
 
+
+//	void									Spawn();
+//	void									CallSpawn();
+
+	bool									IsType( const eDynamicType & classtype ) const;
+	const std::string &						GetClassname() const;
+	const std::string &						GetParentClassname() const;
+
+
+	// Static functions
+	static void								Init();
+	static void								Shutdown();
+	static eDynamicType *					GetTypeByClassname( const char * classname );
+	static eDynamicType *					GetTypeByNumber( int num );
+	static int								GetNumTypes() { return typeNames.size(); }
+
+private:
+
+//	using classSpawnFunc_t = void (eClass::*)();
+
+//	classSpawnFunc_t						CallSpawnFunc( eDynamicType * cls );
+
+	static std::vector<eDynamicType *>		typeNumbers;		// alphabetical order
+	static std::vector<eDynamicType *>		typeNames;			// typeNum order
+	static bool								initialized;
 };
+
+//***********************
+// eDynamicType::IsType
+// returns true if the object's class 
+// is derived from the class defined by param classType
+//***********************
+inline bool eDynamicType::IsType( const eDynamicType & classType ) const {
+	return ( ( typeNum >= classType.typeNum ) && ( typeNum <= classType.lastChild ) );
+}
+
+//***********************
+// eClass::IsType
+// see eDynamicType::IsType
+//***********************
+inline bool eClass::IsType( const eDynamicType & classType ) const {
+	eDynamicType * subClass = GetType();
+	return subClass->IsType( classType );
+}
 
 }	   /* evil */
 #endif /* EVIL_CLASS_H */
--- EngineOfEvil/source/Dictionary.h
+++ EngineOfEvil/source/Dictionary.h
@@ -45,23 +45,23 @@ public:
 
 	bool												IsEmpty() const { return args.empty(); }
 	void												Clear();
-	void												Remove( const char *key );
-	void												Set( const char *key, const char *value );
-	void												SetFloat( const char *key, float val );
-	void												SetInt( const char *key, int val );
-	void												SetBool( const char *key, bool val );
-	void												SetVec2( const char *key, const eVec2 & value );
-	void												SetVec3( const char *key, const eVec3 & value );
-	void												SetVec4( const char *key, const eQuat & value );
+	void												Remove( const char * key );
+	void												Set( const char * key, const char * value );
+	void												SetFloat( const char * key, float value );
+	void												SetInt( const char * key, int value );
+	void												SetBool( const char * key, bool value );
+	void												SetVec2( const char * key, const eVec2 & value );
+	void												SetVec3( const char * key, const eVec3 & value );
+	void												SetVec4( const char * key, const eQuat & value );
 
 														// these return default values of 0.0, 0 and false (based off "")
-	const char *										GetString( const char *key, const char *defaultString = "" ) const;
-	float												GetFloat( const char *key, const char *defaultString ) const;
-	int													GetInt( const char *key, const char *defaultString ) const;
-	bool												GetBool( const char *key, const char *defaultString ) const;
-	eVec2												GetVec2( const char *key, const char *defaultString = nullptr ) const;
-	eVec3												GetVec3( const char *key, const char *defaultString = nullptr) const;
-	eQuat												GetVec4( const char *key, const char *defaultString = nullptr) const;
+	const char *										GetString( const char * key, const char * defaultString = "" ) const;
+	float												GetFloat( const char * key, const char * defaultString ) const;
+	int													GetInt( const char  * key, const char * defaultString ) const;
+	bool												GetBool( const char * key, const char * defaultString ) const;
+	eVec2												GetVec2( const char * key, const char * defaultString = nullptr ) const;
+	eVec3												GetVec3( const char * key, const char * defaultString = nullptr) const;
+	eQuat												GetVec4( const char * key, const char * defaultString = nullptr) const;
 
 
 	typedef std::unordered_map<std::string, std::string>::iterator iterator;
@@ -89,8 +89,8 @@ inline void eDictionary::Clear() {
 //********************
 // eDictionary::Remove
 //********************
-inline void eDictionary::Remove( const char *key ) {
-	if ( key == NULL || key[0] == '\0' )
+inline void eDictionary::Remove( const char * key ) {
+	if ( key == nullptr || key[0] == '\0' )
 		return;
 
 	const auto & iter = args.find(key);
@@ -101,8 +101,8 @@ inline void eDictionary::Remove( const char *key ) {
 //********************
 // eDictionary::Set
 //********************
-inline void eDictionary::Set( const char *key, const char *value ) {
-	if ( key == NULL || key[0] == '\0' )
+inline void eDictionary::Set( const char * key, const char * value ) {
+	if ( key == nullptr || key[0] == '\0' )
 		return;
 
 	args[key] = value;
@@ -111,31 +111,31 @@ inline void eDictionary::Set( const char *key, const char *value ) {
 //********************
 // eDictionary::SetFloat
 //********************
-inline void eDictionary::SetFloat( const char *key, float val ) {
-	snprintf(setBuffer.data(), setBuffer.size(), "%f", val);
+inline void eDictionary::SetFloat( const char * key, float value ) {
+	snprintf(setBuffer.data(), setBuffer.size(), "%f", value);
 	Set( key, setBuffer.data() );
 }
 
 //********************
 // eDictionary::SetInt
 //********************
-inline void eDictionary::SetInt( const char *key, int val ) {
-	snprintf(setBuffer.data(), setBuffer.size(), "%i", val);
+inline void eDictionary::SetInt( const char * key, int value ) {
+	snprintf(setBuffer.data(), setBuffer.size(), "%i", value);
 	Set( key, setBuffer.data() );
 }
 
 //********************
 // eDictionary::SetBool
 //********************
-inline void eDictionary::SetBool( const char *key, bool val ) {
-	snprintf(setBuffer.data(), setBuffer.size(), "%i", val);
+inline void eDictionary::SetBool( const char * key, bool value ) {
+	snprintf(setBuffer.data(), setBuffer.size(), "%i", value);
 	Set( key, setBuffer.data() );
 }
 
 //********************
 // eDictionary::GetString
 //********************
-inline const char * eDictionary::GetString( const char *key, const char *defaultString ) const {
+inline const char * eDictionary::GetString( const char * key, const char * defaultString ) const {
 	const auto & iter = args.find(key);
 	if ( iter != args.end() )
 		return iter->second.c_str();
@@ -146,28 +146,28 @@ inline const char * eDictionary::GetString( const char *key, const char *default
 //********************
 // eDictionary::GetFloat
 //********************
-inline float eDictionary::GetFloat( const char *key, const char *defaultString ) const {
+inline float eDictionary::GetFloat( const char * key, const char * defaultString ) const {
 	return (float)atof( GetString( key, defaultString ) );
 }
 
 //********************
 // eDictionary::GetInt
 //********************
-inline int eDictionary::GetInt( const char *key, const char *defaultString ) const {
+inline int eDictionary::GetInt( const char * key, const char * defaultString ) const {
 	return atoi( GetString( key, defaultString ) );
 }
 
 //********************
 // eDictionary::GetBool
 //********************
-inline bool eDictionary::GetBool( const char *key, const char *defaultString ) const {
+inline bool eDictionary::GetBool( const char * key, const char * defaultString ) const {
 	return ( atoi( GetString( key, defaultString ) ) != 0 );
 }
 
 //********************
 // eDictionary::SetVec2
 //********************
-inline void eDictionary::SetVec2( const char *key, const eVec2 & value ) {
+inline void eDictionary::SetVec2( const char * key, const eVec2 & value ) {
 	snprintf(setBuffer.data(), setBuffer.size(), "%f %f", value.x, value.y);
 	Set( key, setBuffer.data() );
 }
@@ -175,7 +175,7 @@ inline void eDictionary::SetVec2( const char *key, const eVec2 & value ) {
 //********************
 // eDictionary::SetVec3
 //********************
-inline void eDictionary::SetVec3( const char *key, const eVec3 & value ) {
+inline void eDictionary::SetVec3( const char * key, const eVec3 & value ) {
 	snprintf(setBuffer.data(), setBuffer.size(), "%f %f %f", value.x, value.y, value.z);
 	Set( key, setBuffer.data() );
 }
@@ -183,7 +183,7 @@ inline void eDictionary::SetVec3( const char *key, const eVec3 & value ) {
 //********************
 // eDictionary::SetVec4
 //********************
-inline void eDictionary::SetVec4( const char *key, const eQuat & value ) {
+inline void eDictionary::SetVec4( const char * key, const eQuat & value ) {
 	snprintf(setBuffer.data(), setBuffer.size(), "%f %f %f %f", value.x, value.y, value.z, value.w);
 	Set( key, setBuffer.data()  );
 }
